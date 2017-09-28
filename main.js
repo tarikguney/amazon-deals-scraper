1228047c1b25fb5f78e5f31be34ea9276bf6958f
Tarik Guney <tarik.guney@motorolasolutions.com>
2017-09-28 12:26:22 -0600

    Refactored and beautified the code little bit

--- main.js
+++ main.js
@@ -15,28 +15,29 @@ CDP((client) => {
   // Evaluate outerHTML after page has loaded.
   Page.loadEventFired(() => {
 
-    Runtime.evaluate({ expression: "var jq = document.createElement('script');jq.src = \"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\";document.getElementsByTagName('head')[0].appendChild(jq);jQuery.noConflict();" })
+    injectjQuery(Runtime);
+
     Runtime.evaluate({ expression: "window.scrollTo(0, document.body.scrollHeight)" })
     setTimeout(function () {
-      Runtime.evaluate({ expression: 'jQuery(".dealContainer").toArray().length'}).then((a) => {
-        console.log("111111111111----------------------------")
-        for (var b in a){
+      Runtime.evaluate({ expression: 'jQuery(".dealContainer").toArray().length' }).then((a) => {
+
+        console.log("---------------- TRAVERSING ROOT OBJECT ---------------------------")
+        for (var b in a) {
           console.log(b)
         }
-        console.log("22222222222222222----------------------------")
-        for (var b in a.result){
+        console.log("---------------- TRAVERSION RESULT PROPERTY -----------------------")
+        for (var b in a.result) {
           console.log(b)
         }
-        console.log("3333333333333333333----------------------------")
-        for (var b in a.result){
+        console.log("---------------- TRAVERSING VALUES OF RESULT PROPERTY -------------")
+        for (var b in a.result) {
           console.log(a.result[b])
         }
-        console.log("4444444444444444----------------------------")
+        console.log("---------------- SHOWING THE RESULT -------------------------------")
         console.log(a.result.value);
+
+        // writeToFile(a.result.value);
         
-        // fs.writeFile("output.html", , function (error) {
-        //   console.log("Done...");
-        // })
         client.close();
       });
     }, 2000);
@@ -44,3 +45,17 @@ CDP((client) => {
 }).on('error', (err) => {
   console.error('Cannot connect to browser:', err);
 });
+
+function injectjQuery(Runtime) {
+  Runtime.evaluate({ expression: "var jq = document.createElement('script');jq.src = \"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\";document.getElementsByTagName('head')[0].appendChild(jq);jQuery.noConflict();" })
+}
+
+function writeToFile(content) {
+  fs.writeFile("OutputFile.txt", content, function (error) {
+    if (error != null) {
+      console.log(error);
+    } else {
+      console.log("OutputFile.txt has been saved...");
+    }
+  });
+}
